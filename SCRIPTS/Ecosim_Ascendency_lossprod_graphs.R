0e14fc73588507522927a651f651f386697cdb9a
Bia Dias <biadias@uw.edu>
2025-01-12 09:14:17 -0700

    adding the plots and code for figures

--- SCRIPTS/Ecosim_Ascendency_lossprod_graphs.R
+++ SCRIPTS/Ecosim_Ascendency_lossprod_graphs.R
@@ -149,4 +149,98 @@ ggsave(
 )
 
 
+# Overlaid plots
+# MHW analysis with heatwaveR package
 
+OISST_data <- readRDS("DATA/OISST_dt.Rds", refhook = NULL) %>% 
+  rename(t=time, temp=sst)
+OISST_NGA <- OISST_data %>%
+  group_by(t) %>%
+  summarize(temp = mean(temp, na.rm = TRUE))
+
+head(OISST_NGA)
+library(heatwaveR)
+
+ts_nga <- ts2clm(OISST_NGA, climatologyPeriod = c("1982-01-01", "2023-05-08"))
+mhw_nga <- detect_event(ts_nga)
+
+mhw_nga$event$Year <- as.numeric(format(mhw_nga$event$date_peak, "%Y"))
+
+Lossprod_year_mhw <- data_summary %>%
+  ggplot(aes(x = Year, y = AverageLossOfProduction)) +
+  theme_transparent() +
+  geom_line(colour = "#c85200", linewidth = 2) +
+  
+  # Now add geom_lolli() with the correct usage:
+  geom_lolli(
+    data = mhw_nga$event,                  # or whatever your event data is called
+    aes(x = Year, y = intensity_max), # you must supply aes()
+    color = "orange",
+    show.legend = FALSE
+    # optionally: fill = "grey70", n = 3, etc.
+  ) +
+  
+  # Then your annotations:
+  annotate(
+    "rect",
+    xmin = 2013.5,
+    xmax = 2016.5,
+    ymin = -Inf,
+    ymax = Inf,
+    fill = "gray80",
+    alpha = 0.4
+  ) +
+  annotate(
+    "rect",
+    xmin = 2018.5,
+    xmax = 2019.5,
+    ymin = -Inf,
+    ymax = Inf,
+    fill = "gray80",
+    alpha = 0.4
+  ) +
+  annotate(
+    "rect",
+    xmin = 1989,
+    xmax = 1994,
+    ymin = -Inf,
+    ymax = Inf,
+    fill = "darkred",
+    alpha = 0.15
+  ) +
+  geom_vline(
+    xintercept = 1989,
+    color = "darkred",
+    linetype = "dashed",
+    linewidth = 1, 
+    alpha = 0.4
+  ) +
+  scale_y_continuous(
+  name = "Production loss (Biomass mt/km2)",
+  sec.axis = dup_axis(name = "Heatwave intensity (°C)"))+
+    theme(
+      axis.text.x =  element_text(size = 12, color = "black"),
+      axis.title.x = element_text(size = 12, color = "black"),
+      axis.ticks.x = element_line(colour = "darkgrey"),
+      axis.line.x = element_line(colour = "darkgrey"),
+      axis.text.y = element_text(size = 12, color = "black"),
+      axis.title.y = element_text(size = 12, color = "black"),
+      axis.ticks.y = element_line(colour = "darkgrey"),
+      axis.line.y = element_line(colour = "darkgrey"),
+      axis.text.y.right = element_text(size = 12, color = "darkorange"),
+      axis.title.y.right = element_text(size = 12, color = "darkorange"),
+      axis.ticks.y.right = element_line(colour = "darkorange"),
+      axis.line.y.right = element_line(colour = "darkorange"),
+      legend.text = element_text(size = 12, color = "darkgrey"),
+      legend.title = element_text(size = 12, color = "darkgrey")
+    ) 
+
+Lossprod_year_mhw
+
+ggsave(
+  "FIGURES/Lossprod_year_mhw.png",
+  Lossprod_year_mhw,
+  width = 8,
+  height = 4.6,
+  bg = "transparent"
+)
